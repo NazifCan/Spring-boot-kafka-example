dc674d6f05b28d720e12f746cc146ae9ac0bf166
Wolfgang <n.cankabakcioglu@gmail.com>
2023-07-22 00:18:22 +0300

    Spring Boot Kafka Projesi

---
+++ src/test/java/com/newbiecoder/kafka/SpringbootKafkaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newbiecoder.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
